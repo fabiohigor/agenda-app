ae8d59c06978d8d2bba2c3028eef863062ae867b
Fabio <fabioec.2010@gmail.com>
2024-04-14 20:07:19 -0400

    Finalizando criar e favoritar contato

--- src/app/contato.service.ts
+++ src/app/contato.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Contato } from './contato/contato';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContatoService {
+  url: string = environment.apiBaseUrl; 
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+    ) { }
+
+    save(contato: Contato) : Observable<Contato> {
+      return this.http.post<Contato>(this.url, contato);
+    }
+    
+    list() : Observable<Contato[]>{
+      return this.http.get<Contato[]>(this.url);
+    }
+
+    favorite(contato: Contato): Observable<any>{
+      return this.http.patch(`${this.url}/${contato.id}/favorito`, null);
+    }
 }
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from './contato';
+import { ContatoService } from '../contato.service';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+  contatos: Contato[] = [];
+  colunas = ['id', 'nome', 'email', 'favorito'];
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.montarFormulario();
+    this.listarContatos();
+  }
+
+  montarFormulario(){
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.email]
+    })
+  }
+
+  listarContatos(){
+    this.service.list().subscribe(response => {
+      this.contatos = response;
+    })
+  }
+
+  favoritar(contato: Contato){
+  
+    this.service.favorite(contato).subscribe( response =>{
+      contato.favorito = !contato.favorito;
+    })
+    
+  }
+
+  submit(){
+    const formValues = this.formulario.value;
+    const contato : Contato = new Contato(formValues.nome, formValues.email);
+
+    this.service.save(contato)
+      .subscribe( resposta => {
+          let lista : Contato[] =   [... this.contatos, resposta];
+          this.contatos = lista;                    
+      })
   }
 
 }
